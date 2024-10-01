ca67e6acbdd83fd9a2e383a850472b1c6bab5b1d
Ashuraj4321 <145108669+Ashuraj4321@users.noreply.github.com>
2024-10-01 06:57:30 +0530

    Update Day3-chef_working_hours.cpp
    day3 code done

--- Day3-chef_working_hours.cpp
+++ Day3-chef_working_hours.cpp
@@ -1,28 +1,15 @@
-#include <iostream>
+ #include <bits/stdc++.h>
 using namespace std;
 
-int calculateWeeklyHours(int X, int Y) {
-
-    //from Monday to Thursday
-    int weekdaysHours = X * 4;
-    // on Friday (Chill Day)
-    int fridayHours = Y;
-    // Total hours 
-    return weekdaysHours + fridayHours;
-}
-
 int main() {
-    int X, Y;
-    cout << "Enter hours worked from Monday to Thursday (X): ";
-    cin >> X;
-    cout << "Enter hours worked on Friday (Y): ";
-    cin >> Y;
+	// your code goes here
+	int t;
+	cin >> t;
+	
+	for(int i = 0; i < t; i++){
+	    int x,y;
+	    cin >> x >> y;
+	    cout << x*4 + y << endl;
+	}
 
-    if (Y >= X) {
-        cout << "Error: Y must be less than X." << endl;
-    }
-    int totalWorkHours = calculateWeeklyHours(X, Y);
-    cout << "Total work hours in a week: " << totalWorkHours << endl;
-
-    return 0;
 }
