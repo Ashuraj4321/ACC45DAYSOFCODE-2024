3289fdc95529be795dfb03346456743c08b51c26
Ashuraj4321 <145108669+Ashuraj4321@users.noreply.github.com>
2024-10-08 04:51:41 +0530

    Create Day-10_weights.cpp
    day10 code done

---
+++ Day-10_weights.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//code goes from here
+
+int main() {
+    int T;
+    cin >> T;  
+
+    while (T--) {
+        int W, X, Y, Z;
+        cin >> W >> X >> Y >> Z;  
+      
+        if (W == X || W == Y || W == Z || 
+            W == X + Y || W == X + Z || W == Y + Z || 
+            W == X + Y + Z) {
+            cout << "YES" << endl;  
+        } else {
+            cout << "NO" << endl;  
+        }
+    }
+
+    return 0;
+}
